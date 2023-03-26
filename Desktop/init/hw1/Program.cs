c3d8faec4ce93e3278d303fcec15fd259322c7fe
Anar <opofiz1992@gmail.com>
2023-03-26 15:55:26 +0700

    11\14\16задачи

--- Desktop/init/hw1/Program.cs
+++ Desktop/init/hw1/Program.cs
@@ -3,7 +3,7 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-*/
+
 Console.Write("Введите первое число ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число ");
@@ -20,7 +20,6 @@ else {
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
-*/
 
 Console.Write("Введите первое число ");
 int a = int.Parse(Console.ReadLine());
@@ -40,7 +39,7 @@ Console.WriteLine(max);
 4 -> да
 -3 -> нет
 7 -> нет
-*/
+
 
 Console.Write("Введите первое число ");
 int a = int.Parse(Console.ReadLine());
@@ -51,14 +50,15 @@ else {
     Console.Write(" не четное ");
 }
 
-/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 5 -> 2, 4
 8 -> 2, 4, 6, 8
-*/
+
 
 Console.Write("Введите число ");
 int N = int.Parse(Console.ReadLine());
 for (int i = 1; i <= N; i++)
     if (i % 2 == 0)
        Console.Write($"{i} ");
+       */
---
+++ Desktop/init/lessions/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+Random random = new Random();
+int randomValue = random.Next(10,100);
+
+
+int lastDigit = randomValue % 10;
+int firstDigit = randomValue / 10;
+
+Console.WriteLine(randomValue);
+Console.WriteLine(lastDigit);
+Console.WriteLine(firstDigit);
+
+
+if (lastDigit>lastDigit){
+    Console.WriteLine(lastDigit);
+}
+else {
+    Console.WriteLine(firstDigit);
+}
+*/
+
+/* Задача 11 Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+
+Random random = new Random();
+int randomValue = random.Next(100,1000);
+int lastDigit = randomValue % 10;
+int firsttwoDigit = randomValue / 100 * 10;
+int newValue = lastDigit + firsttwoDigit;
+Console.WriteLine(randomValue);
+Console.WriteLine(newValue);
+
+
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+int res = number1 % number2;
+
+if (res == 0 ){
+    Console.Write(res);
+    Console.Write("кратно");
+}
+else {
+     Console.Write(res);  прорешать!!!!
+     Console.Write("не кратно");
+}
+адача No14. Общее обсуждение
+Напишите программу, которая принимает на вход число 
+и проверяет, кратно ли оно одновременно 7 и 23.
+● 14 -> нет
+● 46 -> нет
+● 161 -> да
+
+
+Console.Write("Введите первое число ");
+int num1 = int.Parse(Console.ReadLine());
+
+if ( num1 % 7 == 0 & num1 % 23 == 0){
+    Console.WriteLine($" {num1} -> да");
+}
+else {
+    Console.WriteLine($" {num1} -> нет");
+}
+*/
+
+
+
+/*
+Задача No16. Работа в группах
+Напишите программу, которая принимает на вход
+ два числа и проверяет, является ли одно число 
+ квадратом другого.
+● 5, 25 -> да ● -4, 16 -> да ● 25, 5 -> да ● 8,9 -> нет
+
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if (b == a*a | a == b*b ) {
+    Console.WriteLine ($" {a}, {b} -> да");
+}
+else {
+    Console.WriteLine ($" {a}, {b} -> нет");
+}
+*/
+
