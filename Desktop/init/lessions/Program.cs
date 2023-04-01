d031e4bbbffa745064680a26509b15eff1b67cba
Anar <opofiz1992@gmail.com>
2023-04-01 16:34:04 +0700

    занятие3

--- Desktop/init/lessions/Program.cs
+++ Desktop/init/lessions/Program.cs
@@ -19,7 +19,9 @@ else {
 }
 */
 
-/* Задача 11 Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+/* Задача 11 Напишите программу, 
+которая выводит случайное трехзначное число 
+и удаляет вторую цифру этого числа.
 
 456 -> 46
 782 -> 72
@@ -90,6 +92,133 @@ if (b == a*a | a == b*b ) {
 }
 else {
     Console.WriteLine ($" {a}, {b} -> нет");
+}
+*/
+
+/*Console.WriteLine("Введите координату");
+int x = int.Parse(Console.ReadLine());
+
+int y = int.Parse(Console.ReadLine());
+
+if(x == 0 || y == 0){
+    Console.WriteLine("x или y равны нулю, введите верные данные");
+return;
+}
+
+
+Console.WriteLine("Введите x");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y");
+int y = int.Parse(Console.ReadLine());
+
+int result = GetNumberOfQuarter(x, y);
+if (result == 0){
+    Console.WriteLine("x или y равны нулю");
+    return;
+}
+Console.WriteLine(result);
+
+int GetNumberOfQuarter(int x, int y)
+{
+    if(x > 0 && x > 0)
+    {
+    return 1;
+    }
+    if(x > 0 && y < 0)
+    {
+    return 2;
+    }
+    if(x < 0 && x < 0)
+    {
+    return 3;
+    }
+    if(x > 0 && y < 0)
+    {
+    return 4;
+    }
+return 0;
+}
+
+/*Задача No18. Общее обсуждение
+Напишите программу, 
+которая по заданному номеру четверти,
+показывает диапазон возможных координат точек
+в этой четверти (x и y).
+
+Console.WriteLine("Введите Номер четверти");
+int x = int.Parse(Console.ReadLine());
+string range = GetRangeByQuarter(x);
+
+Console.WriteLine(range);
+
+string GetRangeByQuarter(int x)
+{
+    if (x == 1)
+    {
+        return "x > 0 и y > 0  ";
+    }
+    if (x == 2)
+    {
+        return "x > 0 и y < 0  ";
+    }
+    if (x == 3)
+    {
+        return "x < 0 и y < 0  ";
+    }
+    if (x == 4)
+    {
+        return "x > 0 и y < 0  ";
+    }
+    
+        return " ";
+    
+}
+
+
+
+//AB = √(xb - xa)2 + (yb - ya)2
+
+Math.Sqrt
+Math.Pow(5, 2)
+
+// Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве.
+
+Console.WriteLine("Введите ax");
+double ax = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите ay");
+double ay = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите bx");
+double bx = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите by");
+double by = double.Parse(Console.ReadLine());
+
+double distance = GetDistance(ax, ay, bx, by);
+
+Console.WriteLine($"расстояние между точками {distance}");
+
+double GetDistance (double ax, double ay, double bx, double by)
+{
+    return Math.Sqrt(Math.Pow((bx - ax),2) + Math.Pow((by - ay),2));
+}
+
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+от 1 до N.
+● 5 -> 1, 4, 9, 16, 25.
+● 2 -> 1,4 
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+SqrTabl(a);
+
+void SqrTabl(int a)
+{
+    for (int i = 1; i <= a; i++)
+    {
+        Console.WriteLine(i * i);
+    }
 }
 */
 
