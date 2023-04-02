2ee77f7ec4a81af9a9ae278d619ad21bdae898d8
Anar <opofiz1992@gmail.com>
2023-04-02 15:35:30 +0700

    задач1

--- Desktop/init/lessions/Program.cs
+++ Desktop/init/lessions/Program.cs
@@ -222,3 +222,96 @@ void SqrTabl(int a)
 }
 */
 
+// int number = int.Parse(Console.ReadLine());
+
+// int[] ConvertToArray(int number){
+// int [] result = new int[];
+// }
+// int GetLengthNumber(int number){
+//     int length = 0;
+//     while (number != 0)
+//     {
+//         number = number / 10;
+//         length++;
+//     }
+//     return length;
+// }
+
+// int a = int.Parse(Console)
+// {
+//     int sum = 0;
+//     for (var i = 1; i <= number; i++)
+//     {
+//     sum += i;
+//     }
+// return sum;
+// }
+/*
+/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 5 -> 120
+*/
+/*
+Console.WriteLine("Введите N:");
+int n = int.Parse(Console.ReadLine());
+
+int result = GetComposition(n);
+
+Console.WriteLine(result);
+
+int GetComposition(int number)
+{
+    int result = 1;
+    
+    for(int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+    
+}
+*/
+
+
+/*
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+
+
+Console.WriteLine("Введите N:");
+int stop = int.Parse(Console.ReadLine());
+int start = 1;
+
+int [] arrayend = new int [stop];
+
+arrayend = FillArray(start, stop);
+
+PrintArray(arrayend);
+
+
+// Метод вывода в консоль массива
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++) // создание цикла
+    {
+        Console.Write(array[i]); // Вывод в консоль
+    }
+}
+
+// Метод заполнения массива случайными числами (0, 1)
+int [] FillArray (int start, int stop)
+{
+    int [] array = new int [stop];
+
+    Random random = new Random();
+
+for (int i = start; i < stop; i++)
+    {
+        int randomValue = random.Next(0, 2);
+        array[i] = randomValue;
+    }
+
+    return array;
+}*/
+
